dff60552d7e9575c6e72a339be0c65a7eb4b5926
Marc Gaspà Joval <mgaspjoval2002@gmail.com>
2025-05-01 12:16:24 +0200

    ordre i setup mpi

---
+++ experiment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <program>"
+  exit 1
+fi
+program="$1"
+
+machines=(2 4 8)              # number of nodes to request
+sizes=(100 1000 2000)
+steps_list=(100 1000 10000 100000)
+
+mkdir -p logs
+
+for m in "${machines[@]}"; do
+  for size in "${sizes[@]}"; do
+    for steps in "${steps_list[@]}"; do
+      jobname="${program}_mach${m}_sz${size}_st${steps}"
+      echo "Submitting $jobname (nodes=$m, size=$size, steps=$steps)"
+      qsub \
+        -N "$jobname" \
+        -pe mpich "$m" \            # request m MPI slots → m nodes if allocation_rule=1
+        -o "logs/${jobname}.out" \
+        -e "logs/${jobname}.err" \
+        -cwd \
+        ./hpc.sh "$program" "$size" "$steps" "$m"
+    done
+  done
+done
---
+++ hpc.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+## Specifies the interpreting shell for the job.
+#$ -S /bin/bash
+
+## Specifies that all environment variables active within the qsub utility be exported to the context of the job.
+#$ -V
+
+## Execute the job from the current working directory.
+#$ -cwd
+
+## Parallel programming environment (mpich) to instantiate and number of computing slots.
+#$ -pe mpich 8 
+
+## The  name  of  the  job.
+#$ -N EP_B 
+
+## Send an email at the start and the end of the job.
+#$ -m be
+
+## The email to send the queue manager notifications. 
+#$ -M Your_email@alumnes.udl.cat
+
+## The folders to save the standard and error outputs.
+#$ -o $HOME/NPB3.3.1/NPB3.3-MPI/bin
+#$ -e $HOME
+
+MPICH_MACHINES=$TMPDIR/mpich_machines
+cat $PE_HOSTFILE | awk '{print $1":"$2}' > $MPICH_MACHINES
+
+# now the script
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 <program> <size> <steps> <threads>"
+  exit 1
+fi
+
+program=$1
+size=$2
+steps=$3
+threads=$4
+
+output_file="${program}_th${threads}_sz${size}_st${steps}.bmp"
+
+## In this line you have to write the command that will execute your application.
+mpiexec -f $MPICH_MACHINES -n $NSLOTS ./"$program" "$size" "$steps" "$output_file"
+
+rm -rf $MPICH_MACHINES
+
+
